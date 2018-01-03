d93b220c9a0f72b9ed3f7ec2a1a9ae9f9fe8f06c
zingerll <35074820+zingerll@users.noreply.github.com>
2018-01-03 18:38:19 +0100

    Initial commit

---
+++ auto_anzeige/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace auto_anzeige
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Gauge_Handler gauge;
+        public MainWindow()
+        {
+            InitializeComponent();
+            gauge = new Gauge_Handler();
+            gauge.RaiseTimeEvent += HandleClockEvent;
+        }
+
+        public void printTime(string time)
+        {
+            timeLabel.Content = time;
+        }
+
+        void HandleClockEvent(object sender, CustomEventArgs e)
+        {
+            timeLabel.Content = e.Message;
+        }
+
+    }
+}
---
+++ auto_anzeige/gauge_handler.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Threading;
+
+namespace auto_anzeige
+{
+    class Gauge_Handler
+    {
+        DispatcherTimer clock;
+        public event EventHandler<CustomEventArgs> RaiseTimeEvent;
+        public Gauge_Handler()
+        {
+            clock = new System.Windows.Threading.DispatcherTimer();
+            clock.Tick += new EventHandler(clock_Tick);
+            clock.Interval = new TimeSpan(1);
+            clock.Start();
+            OnRaiseCustomEvent(new CustomEventArgs(getTime()));
+
+        }
+
+
+        private string getTime()
+        {
+            DateTime dt = DateTime.Now;
+            String time = dt.Hour.ToString() + ":" + dt.Minute.ToString();
+            return time;
+        }
+
+        private void clock_Tick(object sender, EventArgs e)
+        {
+            OnRaiseCustomEvent(new CustomEventArgs(getTime()));
+            clock.Interval = new TimeSpan(0, 1, 0);
+        }
+
+        protected virtual void OnRaiseCustomEvent(CustomEventArgs e)
+        {
+            // Make a temporary copy of the event to avoid possibility of
+            // a race condition if the last subscriber unsubscribes
+            // immediately after the null check and before the event is raised.
+            RaiseTimeEvent?.Invoke(this, e);
+
+        }
+
+    }
+    public class CustomEventArgs : EventArgs
+    {
+        public CustomEventArgs(string s)
+        {
+            msg = s;
+        }
+        private string msg;
+        public string Message
+        {
+            get { return msg; }
+        }
+    }
+}
